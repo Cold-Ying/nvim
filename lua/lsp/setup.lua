8dd0d6dcce543ec9f8a27fe05b9c53a3ee6633c7
ying0078 <103869508+ying0078@users.noreply.github.com>
2022-12-20 17:12:43 +0800

    Create setup.lua

---
+++ lua/lsp/setup.lua
@@ -0,0 +1,62 @@
+local mason = require("mason")
+local mason_config = require("mason-lspconfig")
+local lspconfig = require("lspconfig")
+
+if not mason or not mason_config or not lspconfig then
+  return
+end
+
+
+-- :h mason-default-settings
+require("mason").setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
+})
+
+-- mason-lspconfig uses the `lspconfig` server names in the APIs it exposes - not `mason.nvim` package names
+-- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
+require("mason-lspconfig").setup({
+  -- 确保安装，根据需要填写
+  ensure_installed = {
+    "sumneko_lua",
+    "tsserver",
+    "tailwindcss",
+    "bashls",
+    "cssls",
+    "dockerls",
+    "emmet_ls",
+    "html",
+    "jsonls",
+    "pyright",
+    "rust_analyzer",
+    "taplo",
+    "yamlls",
+    "gopls",
+  },
+})
+
+
+-- 安装列表
+-- { key: 语言 value: 配置文件 }
+-- key 必须为下列网址列出的名称
+-- https://github.com/williamboman/nvim-lsp-installer#available-lsps
+local servers = {
+  sumneko_lua = require("lsp.config.lua"), -- lua/lsp/config/lua.lua
+}
+-- 自动安装 Language Servers
+
+for name, config in pairs(servers) do
+  if config ~= nil and type(config) == "table" then
+    -- 自定义初始化配置文件必须实现on_setup 方法
+    config.on_setup(lspconfig[name])
+  else
+    -- 使用默认参数
+    lspconfig[name].setup({})
+  end
+end
+
